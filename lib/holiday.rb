0a1096378933417748f3bacdf147a56605389d0e
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-16 20:28:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   bbq_array = []
   holiday_hash.each do |season, data|
-    season.collect do |holiday, supplies|
+    season.each do |holiday, supplies|
       if supplies.include?('BBQ')
     bbq_array << holiday
       end
