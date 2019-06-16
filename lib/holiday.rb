54e22807356f711209aedbb1b92ea511e3061b7f
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-16 20:46:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   counter = 0
   holiday_hash.collect do |season, data|
     data.each do |holiday, supplies|
+      while counter <= 4
       if supplies.include?('BBQ')
     bbq_array << holiday
       counter = counter +1
